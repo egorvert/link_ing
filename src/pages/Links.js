e441b99eb02fd5cbab9cf11c5413524365cfb871
ec22962 <e.vert@se22.qmul.ac.uk>
2024-01-16 14:34:27 +0000

    Added add link button

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Button from "../components/Button";
 
 const Links = () => {
     return (
-        <div>Hello</div>
+        <div>
+            <Button />
+        </div>
     )
 }
 
